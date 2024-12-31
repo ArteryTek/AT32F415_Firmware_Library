ee4796e77596f23c22b6f8c2a55a3e2388c6604f
Artery-MCU <tristamao@arterytek.com>
2024-12-31 09:21:09 +0800

    update version to v2.1.6

--- project/at_start_f415/examples/usb_device/virtual_comport/inc/usb_conf.h
+++ project/at_start_f415/examples/usb_device/virtual_comport/inc/usb_conf.h
@@ -67,7 +67,7 @@ extern "C" {
 #define OTG_PIN_ID                       GPIO_PINS_10
 
 #define OTG_PIN_SOF_GPIO                 GPIOA
-#define OTG_PIN_SOF_GPIO_CLOCK           CRM_GPIOB_PERIPH_CLOCK
+#define OTG_PIN_SOF_GPIO_CLOCK           CRM_GPIOA_PERIPH_CLOCK
 #define OTG_PIN_SOF                      GPIO_PINS_8
 
 /**
